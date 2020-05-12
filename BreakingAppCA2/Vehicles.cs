8f25db54da2dc8b6c3f7f73296b0012e17eb6056
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2020-05-12 19:00:46 +0100

    WeatherConditionData set up complete along with some tidyup

--- BreakingAppCA2/Vehicles.cs
+++ BreakingAppCA2/Vehicles.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Data.Entity;
 
 namespace BreakingAppCA2
 {
@@ -16,10 +17,11 @@ namespace BreakingAppCA2
         // Properties
 
         public string VehicleType { get;}
-        public enum TypeOfWeather { Dry, Wet, Icy }
+       // public enum TypeOfWeather { Dry, Wet, Icy }
         public double Speed { get; }
-       
 
+        public int VechicleID { get; set; }
+        public virtual Weather Condition { get; set; }
 
 
         // Constructors
@@ -29,9 +31,7 @@ namespace BreakingAppCA2
 
             VehicleType = vehicle;
 
-
         }
-
 
 
         //Methods
@@ -44,7 +44,7 @@ namespace BreakingAppCA2
 
 
 
-        public double GetSpeedInMPH(int speed)
+        public double Get_Speed_InMPH(int speed)
         {
             const double mphToKmph = 1.60934;
             return speed * mphToKmph;
@@ -52,13 +52,13 @@ namespace BreakingAppCA2
 
         }
 
-        public double GetWeatherMulitiplyer(double weatherMulti)
+        public double Get_Weather_Mulitiplyer(double weatherMulti)
         {
 
             return weatherMulti;
         }
 
-        public double GetBreakingDistanceInMPH(int speed, double weatherMulti)
+        public double Get_Breaking_DistanceInMPH(int speed, double weatherMulti)
         {
             // speed entered by the user and the muilplyer is determined by the switch
             double calulatedDistance = 0.00;
@@ -75,49 +75,6 @@ namespace BreakingAppCA2
                     breaking_distance = i;
                 }
 
-                
-                //// switch to assign the muliplyer
-                //switch (speed)
-                //{
-                //    case 20:
-                //        breaking_distance = 2.0;
-
-                //        break;
-                //    case 30:
-                //        breaking_distance = 2.5;
-                //        break;
-                //    case 40:
-                //        breaking_distance = 3.0;
-                //        break;
-                //    case 50:
-                //        breaking_distance = 3.5;
-                //        break;
-                //    case 60:
-                //        breaking_distance = 4.0;
-                //        break;
-                //    case 70:
-                //        breaking_distance = 4.5;
-                //        break;
-                //    case 80:
-                //        breaking_distance = 5.0;
-                //        break;
-                //    case 90:
-                //        breaking_distance = 5.5;
-                //        break;
-                //    case 100:
-                //        breaking_distance = 6.0;
-                //        break;
-                //    case 110:
-                //        breaking_distance = 6.5;
-                //        break;
-                //    case 120:
-                //        breaking_distance = 7.0;
-                //        break;
-                //    default:
-                //        MessageBox.Show("an error has happened");
-                //        break;
-
-                //}
             }
             catch (FormatException)
             {
@@ -131,7 +88,7 @@ namespace BreakingAppCA2
             return calulatedDistance;
         }
 
-        public double GetDistances(double calulatedDistance, double speed)
+        public double Get_Distances(double calulatedDistance, double speed)
         {
             // formula for reaction distance
 
@@ -147,7 +104,7 @@ namespace BreakingAppCA2
 
         }
 
-        public double GetVanDistances(double caluatedDistance)
+        public double Get_Van_Distances(double caluatedDistance)
         {
             const double underLoad = 1.33;
             double underLoadDis = 0;
@@ -159,7 +116,7 @@ namespace BreakingAppCA2
 
 
 
-        public double GetCarLength(double calulatedDistance)
+        public double Get_Car_Length(double calulatedDistance)
         {
             const double carLength = 13.00;
             double totalCarLength = 0;
@@ -170,7 +127,7 @@ namespace BreakingAppCA2
             return totalCarLength;
         }
 
-        public double GetLorryBreakingDistanceInMPH(int speed)
+        public double Get_Lorry_Breaking_Distance_InMPH(int speed)
         {
             // speed entered by the user and the muilplyer is determined by the switch
             double calulatedDistance;
@@ -224,57 +181,85 @@ namespace BreakingAppCA2
             return calulatedDistance;
         }
 
-
-        //Notes:
-
-        // thinking distance is approximately 1 foot for every mph you are travelling at.
-        //Thinking distance + braking distance = overall stopping distance.
-        // Calulation for breaking distances
-        //Starting from 20mph you simply multiply the speed by intervals of 0.5,
-        //beginning with 2, for example, 2, 2.5, 3, 3.5 etc., as follows:
-
-        //20 mph x 2 = 40 feet((12 meters) or 3 car lengths)
-        //30 mph x 2.5 = 75 feet((23 meters) or 6 car lengths)
-        //40 mph x 3 = 120 feet((36.5 meters) or 9 car lengths)
-        //50 mph x 3.5  = 175 feet((53 meters) or 13 car lengths)
-        //60 mph x 4 = 240 feet((73 meters) or 18 car lengths)
-        //70 mph x 4.5 = 315 feet((96 meters) or 24 car lengths)
-
-        //(Average car length = 4 metres (13 feet)
+        public class Weather
+        {
 
 
-        // d=vsquared / 2(coefficent of friction 0.60 dry)(g which is acceleration due to gravity or 9.80)
+            public int WeatherID { get; set; }
+            public string TypeOfWeather { get; set; }
 
-        // distance in meters is speed /3.33 then we change to feet
-        // formula for reaction distance
-        /* Formula: d = (s* r) / 3.6
+            public virtual List<Vehicles> Vehicles { get; set; }
 
-         d = reaction distance in metres(to be calculated).
-         s = speed in km/h.
-         r = fixed a 2sec.
-         3.6 = fixed figure for converting km / h to m / s.
+        }
 
-         Example of calculation with a speed of 50 km / h and a reaction time of 2 second:
+        // this class will set up the tables in a database
+        public class WeatherData : DbContext
+        {
+            // overide the constructor to rename the database
+            public WeatherData(): base("WeatherConditionData") { }
 
-         (50 * 2) / 3.6 = 13.9 metres reaction distance
+            // A table of vehicles
+            public DbSet<Vehicles> Vehicles { get; set; }
+            // A table of weather
+            public DbSet<Weather> Weather { get; set; }
 
-         (speed) ² ÷ 20 + thinking distance = overall stopping distance in feet
+        }
 
-        For stopping distance formula in metres, multiply the result by 0.3
-         The graph below illustrates a motorcycles stopping distances in ideal 
-         dry conditions with speed in mph and km/h and distance in metres and feet. 
-         Braking distances are considerably extended in wet and icy conditions. 
-         The braking distance in green is doubled for wet conditions and is multiplied by 10 in icy conditions.
-
-        lorry stopping distance
-        speed 10mph = 14ft sec =3.0
-        20 = 44 sec=3.5
-        30 = 88 sec = 4.0
-        40 =146 sec = 4.5
-        50 = 220 sec = 5.0
-        60 = 306 sec = 5.5
-        70 = 409 sec = 6.0
-         
-         */
     }
+
+
+   
 }
+
+
+//Notes:
+
+// thinking distance is approximately 1 foot for every mph you are travelling at.
+//Thinking distance + braking distance = overall stopping distance.
+// Calulation for breaking distances
+//Starting from 20mph you simply multiply the speed by intervals of 0.5,
+//beginning with 2, for example, 2, 2.5, 3, 3.5 etc., as follows:
+
+//20 mph x 2 = 40 feet((12 meters) or 3 car lengths)
+//30 mph x 2.5 = 75 feet((23 meters) or 6 car lengths)
+//40 mph x 3 = 120 feet((36.5 meters) or 9 car lengths)
+//50 mph x 3.5  = 175 feet((53 meters) or 13 car lengths)
+//60 mph x 4 = 240 feet((73 meters) or 18 car lengths)
+//70 mph x 4.5 = 315 feet((96 meters) or 24 car lengths)
+
+//(Average car length = 4 metres (13 feet)
+
+
+// d=vsquared / 2(coefficent of friction 0.60 dry)(g which is acceleration due to gravity or 9.80)
+
+// distance in meters is speed /3.33 then we change to feet
+// formula for reaction distance
+/* Formula: d = (s* r) / 3.6
+
+ d = reaction distance in metres(to be calculated).
+ s = speed in km/h.
+ r = fixed a 2sec.
+ 3.6 = fixed figure for converting km / h to m / s.
+
+ Example of calculation with a speed of 50 km / h and a reaction time of 2 second:
+
+ (50 * 2) / 3.6 = 13.9 metres reaction distance
+
+ (speed) ² ÷ 20 + thinking distance = overall stopping distance in feet
+
+For stopping distance formula in metres, multiply the result by 0.3
+ The graph below illustrates a motorcycles stopping distances in ideal 
+ dry conditions with speed in mph and km/h and distance in metres and feet. 
+ Braking distances are considerably extended in wet and icy conditions. 
+ The braking distance in green is doubled for wet conditions and is multiplied by 10 in icy conditions.
+
+lorry stopping distance
+speed 10mph = 14ft sec =3.0
+20 = 44 sec=3.5
+30 = 88 sec = 4.0
+40 =146 sec = 4.5
+50 = 220 sec = 5.0
+60 = 306 sec = 5.5
+70 = 409 sec = 6.0
+
+ */
