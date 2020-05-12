878d5a9d06de8af58379ef34c18861b5324a309f
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2020-05-12 19:27:28 +0100

    added datamanagment to the file to build the database

---
+++ DataManagement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BreakingAppCA2;
+
+
+namespace DataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // the new database
+          
+        }
+    }
+}
